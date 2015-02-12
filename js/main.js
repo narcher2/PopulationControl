2f1969349ba1b1ad4460e4c4d03baeabdd8ab7ce
narcher2 <narcher2@gmu.edu>
2015-02-12 03:29:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ function preload() {
     game.load.atlas('breakout', 'assets/games/breakout/breakout.png', 'assets/games/breakout/breakout.json');
     game.load.image('wood', 'assets/misc/wood.png');
     game.load.image('trampolineh', 'assets/trampolineh.png');
-    game.load.image('cat',  'cat' + (Math.floor((Math.random() * 4) + 1)) +'.png');
+    game.load.image('cat',  'assets/cat' + (Math.floor((Math.random() * 4) + 1)) +'.png');
+    game.load.image('box',  'assets/box.png');
 
 }
 
