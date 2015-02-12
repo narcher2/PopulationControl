c09769ce1f0562f46d106ced619a6cc7d38e7232
narcher2 <narcher2@gmu.edu>
2015-02-12 03:34:30 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -156,7 +156,7 @@ function ballLost () {
         ballOnPaddle = true;
 
         //ball.reset(paddle.body.x + 16, paddle.y - 16);
-        ball.reset(300, 400);
+        ball.reset(400, 300);
         
         ball.animations.stop();
     }
@@ -205,16 +205,16 @@ function ballHitPaddle (_ball, _paddle) {
 
     var diff = 0;
 
-    if (_ball.x < _paddle.x+32)
+    if (_ball.x < _paddle.x+64)
     {
         //  Ball is on the left-hand side of the paddle
-        diff = _paddle.x+32 - _ball.x;
+        diff = _paddle.x+64 - _ball.x;
         _ball.body.velocity.x = (-10 * diff);
     }
-    else if (_ball.x > _paddle.x+32)
+    else if (_ball.x > _paddle.x+64)
     {
         //  Ball is on the right-hand side of the paddle
-        diff = _ball.x -_paddle.x+32;
+        diff = _ball.x -_paddle.x+64;
         _ball.body.velocity.x = (10 * diff);
     }
     else
